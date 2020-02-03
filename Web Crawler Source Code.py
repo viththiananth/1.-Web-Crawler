e732407f5faa42a3e663952a67f16247424c8bb7
viththi <56210820+viththiananth@users.noreply.github.com>
2020-02-03 05:55:05 +0530

    Web Crawler

--- Web Crawler Source Code.py
+++ Web Crawler Source Code.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import operator
-import collections
+from collections import Counter
 
 def webcrawl(url):
     wordlist=[]
@@ -15,5 +15,36 @@ def webcrawl(url):
     for each_text in html_parser.find_all('dev',{'class':'entry-content'}):
         content=each_text.text
         words=content.lower().split()
+
+        for each_word in words:
+            wordlist.append(each_word)
+        clean_wordlist(wordlist)
+
+def clean_wordlist(wordlist):
+    clean_list=[]
+    symbols = '!@#$%^&*()_-+={[}]|\;:"<>?/., '
+
+    for word in wordlist:
+        symbols = '!@#$%^&*()_-+={[}]|\;:"<>?/., '
+        for i in range(0,len(symbols)):
+            word=word.replace(symbols[i],'')
+        if len(word)>0:
+            clean_list.append(word)
+    create_dictionary(clean_list)
+
+def create_dictionary(clean_list):
+    word_count={}
+    for word in clean_list:
+        if word in clean_list:
+            word_count[word]+=1
+        else:
+            word_count[word]=1
+
+c=Counter(word_count)
+top=c.most_common(10)
+print(top)
+
+
+
 
 webcrawl('http://www.github.com')
